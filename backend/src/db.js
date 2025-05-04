4e02581976dc467f7151f9cf0e4694caed9991c4
Mohd-mustafa <mustafamohd50720@gmail.com>
2025-05-04 15:10:54 +0530

    Initial push: backend and frontend code

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,14 +1,15 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Mohd9397085178@',
-  database: 'hajjumrah',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
   queueLimit: 0,
 });
 
